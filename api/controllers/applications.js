a9a43039a0ec0a62cfd0c311d1ee13c140196bb1
Curtis Schlak <curtis.schlak@theironyard.com>
2017-02-02 10:05:07 -0600

    Update controller and tests.

--- api/controllers/applications.js
+++ api/controllers/applications.js
@@ -2,30 +2,80 @@
 
 let applications = {
   '98961': [{
+    id: 443449,
     applicationType: 'auto',
     uri: '/my/applications/443449',
     status: 'submitted',
-    startedOn: '2016-12-02'
+    startedOn: '2016-12-02',
+    amountRequested: 40000
   }, {
+    id: 321684,
     applicationType: 'home',
     uri: '/my/applications/321684',
     status: 'under review',
-    startedOn: '2017-01-09'
+    startedOn: '2017-01-09',
+    amountRequested: 360000
   }],
   '37365': [{
+    id: 0968498,
     applicationType: 'card',
     uri: '/my/applications/0968498',
     status: 'not approved',
-    startedOn: '2016-04-19'
+    startedOn: '2016-04-19',
+    amountRequested: 10000
   }]
 }
 
 module.exports = {
-  list: list
+  list: list,
+  create: create
 };
+
+function pad0(s) {
+  s = s.toString();
+  if (s.length === 1) {
+    return '0' + s
+  }
+  return s;
+}
+
+function now() {
+  let d = new Date();
+  return `${d.getFullYear()}-${pad0(d.getMonth() + 1)}-${pad0(d.getDate())}`;
+}
 
 function list(req, res) {
   let token = req.swagger.params['x-customer-token'].value;
   let list = applications[token] || [];
-  res.json(list);
+  res.json({ applications: list });
+}
+
+function create(req, res) {
+  let submission = req.swagger.params.application.value;
+  let token = req.swagger.params['x-customer-token'].value;
+  let list = applications[token] || [];
+
+  let found = list.find(a => a.applicationType === submission.applicationType);
+
+  if (found) {
+    res.status(409).json({
+      message: `An application of type ${submission.applicationType} is already in process.`,
+      existing: found,
+      submitted: submission
+    });
+  } else {
+    submission.id = Math.floor(Math.random() * 1000000);
+    submission.startedOn = now();
+
+    list.push(submission);
+
+    if (submission.applicationType === 'card') {
+      res.status(202);
+      submission.status = 'submitted';
+    } else {
+      res.status(201);
+      submission.status = 'incomplete';
+    }
+    res.json(submission);
+  }
 }
--- test/api/controllers/applications.js
+++ test/api/controllers/applications.js
@@ -3,46 +3,35 @@ var request = require('supertest');
 var server = require('../../../app');
 
 describe('controllers', function() {
-
-  describe('hello_world', function() {
-
-    describe('GET /hello', function() {
-
-      it('should return a default string', function(done) {
-
+  describe('applications', function() {
+    describe('GET /api/my/applications/v1.0', function() {
+      it('should return an empty list of applications for a user that has no applications', function(done) {
         request(server)
-          .get('/hello')
+          .get('/api/my/applications/v1.0')
           .set('Accept', 'application/json')
+          .set('x-customer-token', 123456)
           .expect('Content-Type', /json/)
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
-
-            res.body.should.eql('Hello, stranger!');
-
+            res.body.should.deepEqual({ applications: [] });
             done();
           });
       });
 
-      it('should accept a name parameter', function(done) {
-
+      it('should return a list of applications for a user that has applications', function(done) {
         request(server)
-          .get('/hello')
-          .query({ name: 'Scott'})
+          .get('/api/my/applications/v1.0')
           .set('Accept', 'application/json')
+          .set('x-customer-token', 98961)
           .expect('Content-Type', /json/)
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
-
-            res.body.should.eql('Hello, Scott!');
-
+            res.body.applications.should.have.length(2);
             done();
           });
       });
-
     });
-
   });
-
 });
